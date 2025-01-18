8af7e60265415087b7030f2e997ebf360a94c96d
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-01-18 20:50:45 +0530

    new

--- app/views.py
+++ app/views.py
@@ -1,32 +1,7 @@
-from flask import render_template, request, redirect, url_for
-from app import app
-import random
+from app import app 
+from flask import render_template
 
-# Predefined mood-based playlists
-playlists = {
-    "happy": ["Happy Song 1", "Happy Song 2", "Happy Song 3"],
-    "sad": ["Sad Song 1", "Sad Song 2", "Sad Song 3"],
-    "relaxed": ["Relaxed Song 1", "Relaxed Song 2", "Relaxed Song 3"],
-    "energetic": ["Energetic Song 1", "Energetic Song 2", "Energetic Song 3"],
-}
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/get-mood", methods=["POST"])
-def get_mood():
-    if request.method == "POST":
-        mood = request.form.get("mood")
-        songs = playlists.get(mood.lower(), [])
-        if songs:
-            return render_template("result.html", mood=mood.capitalize(), songs=songs)
-        return render_template("result.html", mood=mood.capitalize(), songs=["No songs found!"])
-    return redirect(url_for("home"))
-
-@app.route("/sign-up", methods=["GET","POST"])
-def sign_up():
-    if request.method == "POST":
-        email = request.form.get("email")
-        return render_template("sign_up.html", email=email)
-    return redirect(url_for("home"))
