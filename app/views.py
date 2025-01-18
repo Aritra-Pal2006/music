eb9e39a822e29bae079f71954f92db4feca10ba7
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-01-18 21:35:23 +0530

    lol2

--- app/views.py
+++ app/views.py
@@ -3,5 +3,9 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    return render_template('base.html')
+
+@app.route('/about')
+def about():
     return render_template('home.html')
 
